4ec95c29f1fd70296599763d536c0aeb38074d08
Lee <nbl3cj@virginia.edu>
2020-11-12 22:51:12 -0500

    db index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const express = require("express");
+const mysql = require("mysql");
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +12,27 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+const app = express();
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "vxw6ta_a",
+  host: "usersrv01.cs.virginia.edu",
+  password: "F4ll2020!!",
+  database: "vxwt6ta",
+});
+
+
+app.listen(3001, () =>{
+  console.log("running server");
+});
+// $username = 'vxw6ta_a';
+// $password = 'F4ll2020!!';
+// $host = 'usersrv01.cs.virginia.edu';
+// $dbname = 'vxw6ta';
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
