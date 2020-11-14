8345de6c2e819dc68670b77aac30403ba8cced71
Lee <nbl3cj@virginia.edu>
2020-11-13 20:11:56 -0500

    adding cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express') //variable called express, requiring express middleware
 const app = express()
+var cors = require('cors')
 
+app.use(cors());
 app.get('/', (req, res)=> {
     res.send("hello world");
+})
+app.get('/bruh', (req, res)=> {
+    res.send("hasdfd");
 })
 
 app.listen(3001, () => {
